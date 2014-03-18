0a706d001a09973a1be87ad99811707943c7aace
JimSeker <seker@uwyo.edu>
2014-03-17 20:42:05 -0600

    fixed errors in path.

--- WriteContact/src/edu/cs4730/writecontact/MainActivity.java
+++ WriteContact/src/edu/cs4730/writecontact/MainActivity.java
@@ -1,20 +1,10 @@
 package edu.cs4730.writecontact;
 
 import java.util.ArrayList;
-
-
-
-import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.content.ContentProviderOperation;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
-import android.os.Build;
 import android.provider.ContactsContract;
 
 public class MainActivity extends Activity {
