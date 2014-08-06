60fb56db1b44d33d503b2b2fbd64fd9357dfb660
JimSeker <seker@uwyo.edu>
2014-08-05 21:01:03 -0600

    update to 4.4W, added fragment and comments.

--- RunningApp/src/edu/cs4730/runningapp/MainActivity.java
+++ RunningApp/src/edu/cs4730/runningapp/MainActivity.java
@@ -1,81 +1,21 @@
 package edu.cs4730.runningapp;
 
-import java.util.List;
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RecentTaskInfo;
-import android.app.ActivityManager.RunningAppProcessInfo;
-import android.app.ActivityManager.RunningServiceInfo;
-import android.app.ActivityManager.RunningTaskInfo;
-import android.content.Context;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+/*
+ * See the MainFragment for the code on how to see the running apps.
+ */
 
-	Button call;
-	TextView output;
+public class MainActivity extends FragmentActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-        findViewById(R.id.btn).setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				getlist();
-			}
-        });
-		output = (TextView) findViewById(R.id.ouput);
-		
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+			.add(R.id.container, new MainFragment()).commit();
+		}		
 	}
-
-	public void getlist() {
-		
-		ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		// getRunningServices,  getRunningAppProcesses, getRunningTasks, and getRecentTasks 
-	    List<RunningServiceInfo> runServices = activityManager.getRunningServices(Integer.MAX_VALUE);
-		List<RunningAppProcessInfo> runApp = activityManager.getRunningAppProcesses();
-		List<RunningTaskInfo> runTasks = activityManager.getRunningTasks(Integer.MAX_VALUE); 
-		List<RecentTaskInfo> recentTasks = activityManager.getRecentTasks(Integer.MAX_VALUE, ActivityManager.RECENT_WITH_EXCLUDED);
-		
-		output.setText("Running Tasks list:");
-		for (RunningTaskInfo task : runTasks) {
-			output.append("\n"+ task.baseActivity.getPackageName()+"("+ task.baseActivity.getShortClassName()+")");
-		}
-		
-		output.append("\n\nRunning Services list:");
-		for (RunningServiceInfo task : runServices) {
-			output.append("\n"+ task.service.getPackageName());
-		}
-		
-		output.append("\n\nAppProcess list:");
-		for (RunningAppProcessInfo task : runApp) {
-			//output.append("\n"+ task.importanceReasonComponent.getPackageName());
-			output.append("\n"+ task.processName);
-		}
-		
-		output.append("\n\nRecent Task list:");
-		for (RecentTaskInfo task : recentTasks) {
-			//output.append("\n"+ task.origActivity.getPackageName());
-			output.append("\n"+ task.baseIntent.getComponent().getPackageName());
-		}
-		
-		
-		
-	}
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
--- setwallpaper/src/edu/cs4730/setwallpaper/MainActivity.java
+++ setwallpaper/src/edu/cs4730/setwallpaper/MainActivity.java
@@ -1,50 +1,18 @@
 package edu.cs4730.setwallpaper;
 
-import java.io.IOException;
-
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.WallpaperManager;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-	      
-        Button buttonSetWallpaper = (Button)findViewById(R.id.set);
-        ImageView imagePreview = (ImageView)findViewById(R.id.preview);
-
-        imagePreview.setImageResource(R.drawable.ifixedit);
-        
-        buttonSetWallpaper.setOnClickListener(new Button.OnClickListener(){
-			@Override
-			public void onClick(View view) {
-				WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-				try {
-					myWallpaperManager.setResource(R.drawable.ifixedit);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}});
-
-    }
-
-	
-	/*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+			.add(R.id.container, new MainFragment()).commit();
+		}		
 	}
-*/
 }
