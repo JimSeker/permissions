35246ceee1bbdf1c68c94179bff00b24eb3ca193
JimSeker <seker@uwyo.edu>
2016-10-20 15:46:41 -0600

    partial update.  need request permission for phone.

--- detectCalls/app/src/main/java/edu/cs4730/detectcalls/MainActivity.java
+++ detectCalls/app/src/main/java/edu/cs4730/detectcalls/MainActivity.java
@@ -5,15 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 /*
  * There is nothing interesting here.  It's the receivers that you want to look at.
- * 
+ *
+ *
+ * This needs phone permissions!
  */
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
