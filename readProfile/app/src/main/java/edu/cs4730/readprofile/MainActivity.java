d27a9a05b4977680c632ddb0234c365ff4159ae3
JimSeker <seker@uwyo.edu>
2016-10-21 10:24:19 -0600

    updated to API 25 and added checkpremissions.
    also standardized the example.

--- readProfile/app/src/main/java/edu/cs4730/readprofile/MainActivity.java
+++ readProfile/app/src/main/java/edu/cs4730/readprofile/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cs4730.readprofile;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,16 +11,47 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+	public static final int REQUEST_PERM_ACCESS = 1;
+	MainFragment myMainFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-			.add(R.id.container, new MainFragment()).commit();
-		}
-	}
+        myMainFragment = new MainFragment();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, myMainFragment).commit();
+        }
+    }
 
 
+    //handle the response.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERM_ACCESS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    myMainFragment.logger.setText("Contact Read Access: Granted");
+                    myMainFragment.getprofile();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    myMainFragment.logger.setText("Contact Read Access: Not Granted");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
--- readProfile/app/src/main/java/edu/cs4730/readprofile/MainFragment.java
+++ readProfile/app/src/main/java/edu/cs4730/readprofile/MainFragment.java
@@ -1,67 +1,88 @@
 package edu.cs4730.readprofile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * This reads a profile and displays all the information to the screen.
- * 
+ *
+ * We need contact read permission, as it is dangerous.  not profile though.  odd.
  */
 public class MainFragment extends Fragment {
 
+    TextView logger;
+    String TAG = "MainFragment";
 
-	Button call;
-	TextView output;
-	
-	public MainFragment() {
-		// Required empty public constructor
-	}
+    public MainFragment() {
+        // Required empty public constructor
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View myView = inflater.inflate(R.layout.fragment_main, container, false);
-        myView.findViewById(R.id.btn).setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				getprofile();
-			}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+        myView.findViewById(R.id.btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckPerm();
+            }
         });
-		output = (TextView) myView.findViewById(R.id.ouput);
-		
-		return myView;
-	}
+        logger = (TextView) myView.findViewById(R.id.logger);
 
-	 public void getprofile() {
-		 
-		 Cursor c = getActivity().getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null,  null, null, null);
-		 int count = c.getCount();
+        return myView;
+    }
 
-		 String[] columnNames = c.getColumnNames();
 
-		 boolean b = c.moveToFirst();
-		 int position = c.getPosition();
-		// if (count == 1 && position == 0) {
-			 for (int i = 0; i < count; i++) {
-				 for (int j = 0; j < columnNames.length; j++) {
-					 output.append("\n"+
-					    columnNames[j] +" " +
-					    c.getString(c.getColumnIndex(columnNames[j]))
-					  );
-				 }	
-				 boolean b2 = c.moveToNext();
-			 }
-		// }
-		 c.close();
-	}
-	
+    //ask for permissions when we start.
+
+    public void CheckPerm() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            //I'm on not explaining why, just asking for permission.
+            Log.v(TAG, "asking for permissions");
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS},
+                    MainActivity.REQUEST_PERM_ACCESS);
+
+        } else {
+            logger.setText("Contact Read Access: Granted\n");
+            getprofile();
+        }
+
+    }
+
+
+    public void getprofile() {
+
+        Cursor c = getActivity().getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, null, null);
+        int count = c.getCount();
+
+        String[] columnNames = c.getColumnNames();
+
+        boolean b = c.moveToFirst();
+        int position = c.getPosition();
+        // if (count == 1 && position == 0) {
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < columnNames.length; j++) {
+                logger.append("\n" +
+                        columnNames[j] + " " +
+                        c.getString(c.getColumnIndex(columnNames[j]))
+                );
+            }
+            boolean b2 = c.moveToNext();
+        }
+        // }
+        c.close();
+    }
+
 }
