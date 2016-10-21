3b3f6667e4cf01ca0f912908b1df279c921a46fc
JimSeker <seker@uwyo.edu>
2016-10-21 08:47:52 -0600

    updated 25 and added Read Permissions.

--- ReadContact/app/src/main/java/edu/cs4730/readcontact/MainActivity.java
+++ ReadContact/app/src/main/java/edu/cs4730/readcontact/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cs4730.readcontact;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,17 +8,47 @@ import android.support.v7.app.AppCompatActivity;
  * See the MainFragment on how to read all the contacts on the phone.
  */
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_PERM_ACCESS = 1;
+    MainFragment myMainFragment;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myMainFragment = new MainFragment();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new MainFragment()).commit();
-		}
-	}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, myMainFragment).commit();
+        }
+    }
 
+    //handle the response.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERM_ACCESS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    myMainFragment.logger.setText("Contact Read Access: Granted");
+                    myMainFragment.fetchContacts();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    myMainFragment.logger.setText("Contact Read Access: Not Granted");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
 }
