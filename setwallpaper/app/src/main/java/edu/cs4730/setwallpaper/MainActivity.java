be247c759496e54e19a19d0f7373967dc89d703f
Jim Ward <seker@uwyo.edu>
2022-04-14 11:31:43 -0600

    simplified the example.

--- setwallpaper/app/src/main/java/edu/cs4730/setwallpaper/MainActivity.java
+++ setwallpaper/app/src/main/java/edu/cs4730/setwallpaper/MainActivity.java
@@ -1,20 +1,44 @@
 package edu.cs4730.setwallpaper;
 
+import android.annotation.SuppressLint;
+import android.app.WallpaperManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-//nothing to see here, check the MainFragment.
-//No wall paper permission is not "dangerous", so no extra check is needed.
+/**
+ * This is an example of how to setup the wall paper on the device.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+	@SuppressLint("ResourceType")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new MainFragment()).commit();
-		}		
+		Button buttonSetWallpaper =findViewById(R.id.set);
+		ImageView imagePreview = findViewById(R.id.preview);
+
+		imagePreview.setImageResource(R.raw.ifixedit);
+
+		buttonSetWallpaper.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+				try {
+					//studio claims there is an error, but compiles and the project runs just fine.  stupid studio.
+					myWallpaperManager.setResource(R.raw.ifixedit);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
 	}
 }
